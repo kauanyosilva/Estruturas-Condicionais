7773dccf2424a3c309a1a921dfbbdf8137db10d9
kauanyosilva <kauanyoliveiradasilvaa@gmail.com>
2024-01-24 17:28:23 -0300

    adicionei

---
+++ estruturacondicionais.js
@@ -0,0 +1,11 @@
+let numero = prompt("digite um numero:");
+
+if (numero >0 ){
+console.log("o numero e positivo.");
+}
+  else if (numero < 0) {
+  console.log("o numero e negativo");
+  }
+else { 
+  console.log('o numero e zero');
+}
